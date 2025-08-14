14707d52427369f4c72eb067f4f9f30fc2f50213
Esteban V.M. <estebanvm90@outlook.com>
2025-08-14 12:46:24 -0300

    update global types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,6 +3,13 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface IntrinsicElements extends React.JSX.IntrinsicElements {}
   }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_API_KEY: string
+      NEXT_PUBLIC_BASE_URL: string
+    }
+  }
 }
 
 export {}
