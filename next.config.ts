bea2da8d3a101189144bde280391c7d85019983e
Esteban V.M. <estebanvm90@outlook.com>
2025-08-14 16:32:49 -0300

    update nextConfig
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'yt3.ggpht.com' },
+      { protocol: 'https', hostname: 'i.ytimg.com' },
+    ],
+  },
 }
 
 export default nextConfig
