d3193218b9978c61eb344ce705f7cba3f89d918f
Esteban V.M. <estebanvm90@outlook.com>
2025-08-14 12:46:16 -0300

    create axios utils
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/lib/axios-utils.ts
@@ -0,0 +1,8 @@
+import type { AxiosInstance } from 'axios'
+import axios from 'axios'
+
+export const getInstance = (): AxiosInstance => {
+  return axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  })
+}
