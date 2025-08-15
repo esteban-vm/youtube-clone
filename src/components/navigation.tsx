aa09e546395ae8ce2c973c67ff54783f35937109
Esteban V.M. <estebanvm90@outlook.com>
2025-08-15 12:17:46 -0300

    update navigation
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -16,34 +16,33 @@ export interface NavigationProps {
 export function Navigation({ children }: NavigationProps) {
   const id = useId()
   const { theme } = useTheme()
-  const ref = useRef<HTMLButtonElement>(null!)
+  const ref = useRef<HTMLDivElement>(null!)
   const [isOpen, setIsOpen] = useState(false)
   const [isClient, setIsClient] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+  const close = () => setIsOpen(false)
 
   useEffect(() => {
     setIsClient(true)
   }, [])
 
   useClickOutside([ref], (isOutside) => {
-    if (isOutside) setIsOpen(false)
+    if (isOutside) close()
   })
 
   if (!isClient) return null
 
-  const toggleOpen = () => {
-    setIsOpen(!isOpen)
-  }
-
   return (
-    <Drawer className='sticky top-0 z-10 h-screen w-full overflow-hidden rounded' id={id} open={isOpen} theme={theme}>
+    <Drawer className='sticky top-0 z-10 h-screen w-full overflow-hidden' id={id} open={isOpen} theme={theme}>
       <Drawer.Content className='flex flex-col'>
         <Navbar as='nav' className='w-full bg-base-300' vanilla>
-          <div className='flex-none lg:hidden'>
-            <Button ref={ref} shape='square' ghost onClick={toggleOpen}>
+          <div className='flex-none'>
+            <Button shape='square' ghost onClick={toggle}>
               <BiMenuAltLeft className='inline-block size-3/4 stroke-current' />
             </Button>
           </div>
-          <div className='mx-2 flex-1 px-2 text-xl font-semibold'>
+          <div className='mx-2 flex-1 px-2 text-lg font-semibold'>
             <Image alt='logo' className='inline-block size-7' src={logoSmall} /> VidTube
           </div>
           <div className='hidden flex-none lg:block'>
@@ -55,10 +54,10 @@ export function Navigation({ children }: NavigationProps) {
         </Navbar>
         <div className='flex flex-grow items-center justify-center'>{children}</div>
       </Drawer.Content>
-      <Drawer.Side className='absolute h-full' drawerId={id}>
+      <Drawer.Side ref={ref} className='absolute h-full' drawerId={id}>
         <Menu as='menu' className='min-h-full w-80 border border-primary bg-base-200 p-4' vanilla>
-          <Menu.Item>Sidebar Item 1</Menu.Item>
-          <Menu.Item>Sidebar Item 2</Menu.Item>
+          <Menu.Item onClick={close}>Sidebar Item 1</Menu.Item>
+          <Menu.Item onClick={close}>Sidebar Item 2</Menu.Item>
         </Menu>
       </Drawer.Side>
     </Drawer>
