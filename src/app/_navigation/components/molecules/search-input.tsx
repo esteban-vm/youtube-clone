1cceb871ee7b28ab644911772bd0417112efe5a3
Esteban V.M. <estebanvm90@outlook.com>
2025-08-16 16:23:21 -0300

    update search input
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/search-input.tsx
+++ src/app/_navigation/components/molecules/search-input.tsx
@@ -3,9 +3,9 @@ import { Input } from 'rsc-daisyui'
 
 export function SearchInput() {
   return (
-    <Input as='label' className='w-full max-w-xl !rounded-3xl'>
-      <input autoComplete='off' className='peer' placeholder='Search' type='search' />
-      <LuSearch className='hidden text-lg opacity-50 peer-placeholder-shown:block' />
+    <Input as='label' className='w-full max-w-lg !rounded-3xl'>
+      <input autoComplete='off' className='peer' placeholder='Buscar…' type='search' />
+      <LuSearch aria-label='icon' className='hidden text-lg opacity-50 peer-placeholder-shown:block' role='img' />
     </Input>
   )
 }
