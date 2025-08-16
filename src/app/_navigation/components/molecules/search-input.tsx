da21c245cfffc7b81d5a7343d6415c0de8945de4
Esteban V.M. <estebanvm90@outlook.com>
2025-08-16 20:57:47 -0300

    testing search input
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/search-input.tsx
+++ src/app/_navigation/components/molecules/search-input.tsx
@@ -1,11 +1,30 @@
+import type { FormEventHandler } from 'react'
+import { useRouter } from 'next/navigation'
+import { useRef } from 'react'
 import { LuSearch } from 'react-icons/lu'
 import { Input } from 'rsc-daisyui'
 
 export function SearchInput() {
+  const router = useRouter()
+  const inputRef = useRef<HTMLInputElement>(null!)
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault()
+    const searchQuery = inputRef.current.value.trim()
+
+    if (searchQuery) {
+      router.push(`/query/${encodeURI(searchQuery)}`)
+    }
+
+    inputRef.current.value = ''
+  }
+
   return (
-    <Input as='label' className='w-full max-w-lg !rounded-3xl !outline-none'>
-      <input autoComplete='off' className='peer' placeholder='Buscar…' type='search' />
-      <LuSearch aria-label='icon' className='hidden text-lg opacity-50 peer-placeholder-shown:block' role='img' />
-    </Input>
+    <form onSubmit={handleSubmit}>
+      <Input as='label' className='w-full max-w-lg !rounded-3xl !outline-none'>
+        <input ref={inputRef} autoComplete='off' className='peer' placeholder='Buscar…' type='search' />
+        <LuSearch aria-label='icon' className='hidden text-lg opacity-50 peer-placeholder-shown:block' role='img' />
+      </Input>
+    </form>
   )
 }
--- src/app/query/[queryId]/page.tsx
+++ src/app/query/[queryId]/page.tsx
@@ -1,7 +1,14 @@
-export default function Page() {
+export interface Props {
+  params: Promise<{ queryId: string }>
+}
+
+export default async function Page({ params }: Props) {
+  const { queryId } = await params
+
   return (
     <section>
       <span>Query Page</span>
+      <p>{decodeURI(queryId)}</p>
     </section>
   )
 }
