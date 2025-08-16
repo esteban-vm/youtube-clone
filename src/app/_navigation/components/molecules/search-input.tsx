e7034fc4723ef068ea71667a40fb2c1d1ec2a62b
Esteban V.M. <estebanvm90@outlook.com>
2025-08-16 19:59:30 -0300

    update style for search input
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/search-input.tsx
+++ src/app/_navigation/components/molecules/search-input.tsx
@@ -3,7 +3,7 @@ import { Input } from 'rsc-daisyui'
 
 export function SearchInput() {
   return (
-    <Input as='label' className='w-full max-w-lg !rounded-3xl'>
+    <Input as='label' className='w-full max-w-lg !rounded-3xl !outline-none'>
       <input autoComplete='off' className='peer' placeholder='Buscar…' type='search' />
       <LuSearch aria-label='icon' className='hidden text-lg opacity-50 peer-placeholder-shown:block' role='img' />
     </Input>
