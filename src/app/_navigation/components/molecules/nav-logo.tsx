a18dbb7876c668cb236036630a41b69791ecd457
Esteban V.M. <estebanvm90@outlook.com>
2025-08-16 16:13:41 -0300

    add link to nav logo
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/nav-logo.tsx
+++ src/app/_navigation/components/molecules/nav-logo.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import logoSmall from '@/assets/logo-small.png'
 
 export function NavLogo() {
   return (
-    <div>
-      <Image alt='logo' className='inline size-9 lg:mr-1' src={logoSmall} />
-      <span className='hidden align-middle font-semibold select-none lg:inline'>VidTube</span>
-    </div>
+    <Link href='/'>
+      <div>
+        <Image alt='logo' className='inline size-9 lg:mr-1' src={logoSmall} />
+        <span className='hidden align-middle font-semibold select-none lg:inline'>VidTube</span>
+      </div>
+    </Link>
   )
 }
