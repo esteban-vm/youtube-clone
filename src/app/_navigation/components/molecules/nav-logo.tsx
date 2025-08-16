3c5cb17574bb969d2ebe4ea6166c996b0af48ccb
Esteban V.M. <estebanvm90@outlook.com>
2025-08-16 15:53:15 -0300

    update nav logo
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/nav-logo.tsx
+++ src/app/_navigation/components/molecules/nav-logo.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image'
 import logoSmall from '@/assets/logo-small.png'
 
-// import logo from '@/assets/logo.png'
-
 export function NavLogo() {
   return (
     <div>
-      {/* <Image alt='logo' className='mr-1 inline size-9 lg:hidden' src={logoSmall} /> */}
-      {/* <Image alt='logo' className='hidden h-9 w-auto lg:mr-1 lg:inline' src={logo} /> */}
       <Image alt='logo' className='inline size-9 lg:mr-1' src={logoSmall} />
-      <span className='hidden align-middle font-semibold lg:inline'>VidTube</span>
+      <span className='hidden align-middle font-semibold select-none lg:inline'>VidTube</span>
     </div>
   )
 }
