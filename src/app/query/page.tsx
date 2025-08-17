2256e9b8cb052721a905a03a3f0915185e24994a
Esteban V.M. <estebanvm90@outlook.com>
2025-08-17 11:01:11 -0300

    update query page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from 'next/navigation'
+
 export interface Props {
-  searchParams: Promise<{ q: string }>
+  searchParams: Promise<{ q?: string }>
 }
 
 export default async function Page({ searchParams }: Props) {
   const { q } = await searchParams
+
+  if (!q) notFound()
 
   return (
     <section>
