4b71f1ec0441b123b0b5ccb361b0dddf48d3d0c6
Esteban V.M. <estebanvm90@outlook.com>
2025-08-21 17:08:57 -0300

    update use fetch hook
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -2,16 +2,22 @@ import axios from 'axios'
 import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
-export interface FetchState<T> {
-  data: T | null
+export type DataState<T> = T | null
+export type ErrorState = string | null
+
+export interface FetchStateV2<T> {
   loading: boolean
-  error: string | null
+  data: DataState<T>
+  error: ErrorState
 }
 
-export const useFetch = <T>(url: string) => {
+export const useFetchV2 = <T>(url: string): FetchStateV2<T> => {
   const router = useRouter()
   const pathname = usePathname()
-  const [state, setState] = useState<FetchState<T>>({ data: null, loading: true, error: null })
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<DataState<T>>(null)
+  const [error, setError] = useState<ErrorState>(null)
 
   useEffect(() => {
     if (pathname !== '/') {
@@ -21,24 +27,35 @@ export const useFetch = <T>(url: string) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setState({ data: null, loading: true, error: null })
-
-      if (process.env.NODE_ENV === 'development') {
-        await new Promise((resolve) => setTimeout(resolve, 5_000))
-      }
-
       try {
-        const { data } = await axios.get<T>(url, { baseURL: process.env.NEXT_PUBLIC_BASE_URL })
-        setState({ data, loading: false, error: null })
+        setLoading(true)
+        setData(null)
+        setError(null)
+
+        if (process.env.NODE_ENV === 'development') {
+          await new Promise((resolve) => setTimeout(resolve, 5_000))
+        }
+
+        const { data, status } = await axios.get<T>(url, {
+          baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+        })
+
+        if (status === 200) {
+          setData(data)
+          setError(null)
+        }
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          setState({ data: null, loading: false, error: error.message })
+          setData(null)
+          setError(error.message)
         }
+      } finally {
+        setLoading(false)
       }
     }
 
     fetchData()
   }, [url])
 
-  return state
+  return { loading, data, error }
 }
