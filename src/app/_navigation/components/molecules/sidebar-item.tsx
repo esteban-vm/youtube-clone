3be8793d202be9fbc157fe39c99c34c6f8979c5f
Esteban V.M. <estebanvm90@outlook.com>
2025-08-22 16:23:48 -0300

    update sidebar item
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/sidebar-item.tsx
+++ src/app/_navigation/components/molecules/sidebar-item.tsx
@@ -1,16 +1,23 @@
 import type { MenuItemProps } from '@/types'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 import { Menu } from 'rsc-daisyui'
 import { useSidebarStore } from '@/hooks'
 
 export function SidebarItem(props: MenuItemProps) {
-  const { drawerId, currentItem, setCurrentItem } = useSidebarStore()
   const { id, label, icon: Icon, link = '/' } = props
+
+  const router = useRouter()
+  const pathname = usePathname()
+  const { drawerId, currentItem, setCurrentItem } = useSidebarStore()
 
   const handleClick = () => {
     setCurrentItem(id)
+
     const checkbox = document.getElementById(drawerId) as HTMLInputElement
     checkbox.checked = false
+
+    if (pathname !== '/') router.push('/')
   }
 
   return (
