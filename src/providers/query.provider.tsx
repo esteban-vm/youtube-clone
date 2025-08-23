3a8ce25e88c3a0a464d67e4731d6b9d45357c46e
Esteban V.M. <estebanvm90@outlook.com>
2025-08-23 11:08:33 -0300

    create query provider
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/providers/query.provider.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export function QueryProvider({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
