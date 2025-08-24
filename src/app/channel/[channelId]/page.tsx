7b624f254da2ef29775b71be525fcbb78bbc6751
Esteban V.M. <estebanvm90@outlook.com>
2025-08-24 15:35:39 -0300

    update channel page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/channel/[channelId]/page.tsx
+++ src/app/channel/[channelId]/page.tsx
@@ -1,7 +1,14 @@
-export default function Page() {
+export interface Props {
+  params: Promise<{ channelId: string }>
+}
+
+export default async function Page({ params }: Props) {
+  const { channelId } = await params
+
   return (
     <section>
-      <span>Channel Page</span>
+      <h2>Channel Page</h2>
+      <p>Channel: {channelId}</p>
     </section>
   )
 }
