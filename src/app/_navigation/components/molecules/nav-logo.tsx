fa495bb1a6a5f59d5791d065af14fd344e179425
Esteban V.M. <estebanvm90@outlook.com>
2025-08-24 15:55:58 -0300

    update nav logo
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/nav-logo.tsx
+++ src/app/_navigation/components/molecules/nav-logo.tsx
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Atoms } from '@/_navigation/components'
 import logoSmall from '@/assets/logo-small.png'
+import { useSidebarStore } from '@/hooks'
 
 export function NavLogo() {
+  const setItem = useSidebarStore((state) => state.setCurrentItem)
+
   return (
     <Link href='/'>
-      <div className='relative size-9'>
+      <div className='relative size-9' onClick={() => setItem(0)}>
         <Image alt='logo' src={logoSmall} fill />
         <Atoms.LogoText>VidTube</Atoms.LogoText>
       </div>
