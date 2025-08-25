10fc77e0391266e0a6aab61ff35459533f10c1c6
Esteban V.M. <estebanvm90@outlook.com>
2025-08-25 15:00:07 -0300

    update thumbnail image
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_components/organisms/video-card.tsx
+++ src/app/_components/organisms/video-card.tsx
@@ -15,7 +15,7 @@ export function VideoCard({ item }: VideoCardProps) {
     <Card className='gap-1 overflow-hidden shadow-sm dark:rounded-lg'>
       <Molecules.CardImage
         duration={contentDetails.duration}
-        image={snippet.thumbnails.high.url}
+        image={snippet.thumbnails.standard.url}
         link={`/video/${snippet.categoryId}/${id}`}
         title={snippet.title}
       />
