039dc127f7ffc09e07cfd8b8ceff902304eb258c
Esteban V.M. <estebanvm90@outlook.com>
2025-08-28 16:26:28 -0300

    update sidebar item
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/_navigation/components/molecules/sidebar-item.tsx
+++ src/app/_navigation/components/molecules/sidebar-item.tsx
@@ -8,7 +8,7 @@ export interface SidebarItemProps extends Props.BaseMenuItem {
   categoryId: `${number}`
 }
 
-export function SidebarItem({ id, categoryId, label, icon: Icon, link = '/' }: SidebarItemProps) {
+export function SidebarItem({ categoryId, label, icon: Icon, link = '/' }: SidebarItemProps) {
   const router = useRouter()
   const pathname = usePathname()
   const { drawerId, currentItem, setCurrentItem } = useSidebarStore()
@@ -21,7 +21,7 @@ export function SidebarItem({ id, categoryId, label, icon: Icon, link = '/' }: S
   }
 
   return (
-    <Menu.Item active={currentItem === id} as={Link} href={link} onClick={handleClick}>
+    <Menu.Item active={currentItem === categoryId} as={Link} href={link} onClick={handleClick}>
       <Icon aria-label='icon' className='size-4' role='img' />
       {label}
     </Menu.Item>
