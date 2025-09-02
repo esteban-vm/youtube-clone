e8e0faa388c105780198891a3d4d180f7cb0b9b4
Esteban V.M. <estebanvm90@outlook.com>
2025-09-02 16:17:19 -0300

    start working on video player
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/video/[categoryId]/[videoId]/_components/organisms/video-player.tsx
+++ src/app/video/[categoryId]/[videoId]/_components/organisms/video-player.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import type { APIResponse } from '@/types'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { api } from '@/services'
+
 export interface VideoPlayerProps {
   videoId: string
 }
 
 export function VideoPlayer({ videoId }: VideoPlayerProps) {
+  const params = {
+    id: videoId,
+    part: ['snippet', 'contentDetails', 'statistics'].toString(),
+  }
+
+  const { data } = useQuery({
+    queryKey: ['Video Detail', videoId],
+    queryFn: () => api.makeRequest<APIResponse.VideoList>('videos', params),
+  })
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return <p>Video: {videoId}</p>
 }
