b3dad92e2b8b68f77f856340483a0b277d58d98b
Esteban V.M. <estebanvm90@outlook.com>
2025-09-09 16:17:46 -0300

    create loading for recommendations page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/movie/[categoryId]/[videoId]/@recommendations/loading.tsx
@@ -0,0 +1,47 @@
+import { Skeleton } from 'rsc-daisyui'
+import tw from 'tailwind-styled-components'
+
+export default function Loading() {
+  return (
+    <>
+      {Array(2)
+        .fill(null)
+        .map(() => (
+          <LoadingItem key={crypto.randomUUID()} />
+        ))}
+    </>
+  )
+}
+
+function LoadingItem() {
+  return (
+    <$.Container>
+      <Skeleton className='aspect-video w-2/5 rounded-lg' />
+      <$.Content>
+        <Skeleton className='h-4 w-full rounded-lg' />
+        <Skeleton className='h-4 w-1/2 rounded-lg' />
+        <Skeleton className='h-4 w-1/2 rounded-lg' />
+      </$.Content>
+    </$.Container>
+  )
+}
+
+function $() {}
+
+$.Container = tw.div`
+  flex
+  h-fit
+  w-full
+  max-w-xl
+  gap-1
+  rounded-lg
+`
+
+$.Content = tw.div`
+  flex
+  w-3/5
+  flex-col
+  gap-0.5
+  p-1
+  pl-0
+`
