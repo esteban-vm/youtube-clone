683fbb65a441727e5003675f941414584e64973f
Esteban V.M. <estebanvm90@outlook.com>
2025-09-09 16:20:42 -0300

    update types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -259,132 +259,132 @@ import type { IconType } from 'react-icons'
 //   }
 // }
 
-export namespace APIResponse {
-  declare interface ChannelItem {
-    kind: string
-    etag: string
-    id: string
-    snippet: {
-      title: string
-      description: string
-      customUrl: string
-      publishedAt: string
-      thumbnails?: {
-        default?: {
-          url: string
-          width: number
-          height: number
-        }
-        medium?: {
-          url: string
-          width: number
-          height: number
-        }
-        high?: {
-          url: string
-          width: number
-          height: number
-        }
-      }
-      localized: {
-        title: string
-        description: string
-      }
-      country: string
-    }
-    contentDetails: {
-      relatedPlaylists: {
-        likes: string
-        uploads: string
-      }
-    }
-  }
+// export namespace APIResponse {
+//   declare interface ChannelItem {
+//     kind: string
+//     etag: string
+//     id: string
+//     snippet: {
+//       title: string
+//       description: string
+//       customUrl: string
+//       publishedAt: string
+//       thumbnails?: {
+//         default?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         medium?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         high?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//       }
+//       localized: {
+//         title: string
+//         description: string
+//       }
+//       country: string
+//     }
+//     contentDetails: {
+//       relatedPlaylists: {
+//         likes: string
+//         uploads: string
+//       }
+//     }
+//   }
 
-  declare interface ChannelList {
-    kind: string
-    etag: string
-    items: ChannelItem[]
-    pageInfo: {
-      totalResults: number
-      resultsPerPage: number
-    }
-  }
+//   declare interface ChannelList {
+//     kind: string
+//     etag: string
+//     items: ChannelItem[]
+//     pageInfo: {
+//       totalResults: number
+//       resultsPerPage: number
+//     }
+//   }
 
-  declare interface VideoItem {
-    kind: string
-    etag: string
-    id: string
-    snippet: {
-      publishedAt: string
-      channelId: string
-      title: string
-      description: string
-      thumbnails?: {
-        default?: {
-          url: string
-          width: number
-          height: number
-        }
-        medium?: {
-          url: string
-          width: number
-          height: number
-        }
-        high?: {
-          url: string
-          width: number
-          height: number
-        }
-        standard?: {
-          url: string
-          width: number
-          height: number
-        }
-        maxres?: {
-          url: string
-          width: number
-          height: number
-        }
-      }
-      channelTitle: string
-      tags?: string[]
-      categoryId: string
-      liveBroadcastContent: string
-      localized: {
-        title: string
-        description: string
-      }
-      defaultAudioLanguage: string
-      defaultLanguage?: string
-    }
-    contentDetails: {
-      duration: string
-      dimension: string
-      definition: string
-      caption: string
-      licensedContent: boolean
-      contentRating: object
-      projection: string
-    }
-    statistics: {
-      viewCount: string
-      likeCount: string
-      favoriteCount: string
-      commentCount: string
-    }
-  }
+//   declare interface VideoItem {
+//     kind: string
+//     etag: string
+//     id: string
+//     snippet: {
+//       publishedAt: string
+//       channelId: string
+//       title: string
+//       description: string
+//       thumbnails?: {
+//         default?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         medium?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         high?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         standard?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//         maxres?: {
+//           url: string
+//           width: number
+//           height: number
+//         }
+//       }
+//       channelTitle: string
+//       tags?: string[]
+//       categoryId: string
+//       liveBroadcastContent: string
+//       localized: {
+//         title: string
+//         description: string
+//       }
+//       defaultAudioLanguage: string
+//       defaultLanguage?: string
+//     }
+//     contentDetails: {
+//       duration: string
+//       dimension: string
+//       definition: string
+//       caption: string
+//       licensedContent: boolean
+//       contentRating: object
+//       projection: string
+//     }
+//     statistics: {
+//       viewCount: string
+//       likeCount: string
+//       favoriteCount: string
+//       commentCount: string
+//     }
+//   }
 
-  declare interface VideoList {
-    kind: string
-    etag: string
-    items: VideoItem[]
-    nextPageToken: string
-    pageInfo: {
-      totalResults: number
-      resultsPerPage: number
-    }
-  }
-}
+//   declare interface VideoList {
+//     kind: string
+//     etag: string
+//     items: VideoItem[]
+//     nextPageToken: string
+//     pageInfo: {
+//       totalResults: number
+//       resultsPerPage: number
+//     }
+//   }
+// }
 
 export namespace Props {
   declare interface WithChildren<T extends object = unknown> extends T {
