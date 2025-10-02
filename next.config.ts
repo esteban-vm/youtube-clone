86ce6562687cb469b16c6b3ae57c1bbc8174e082
Esteban V.M. <estebanvm90@outlook.com>
2025-10-01 22:07:02 -0300

    update next config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'yt3.ggpht.com' },
       { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: 'yt3.ggpht.com' },
+      { protocol: 'https', hostname: 'yt3.googleusercontent.com' },
     ],
   },
 }
