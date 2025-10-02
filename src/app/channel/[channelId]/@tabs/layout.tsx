c822590957057534125e85377de94a327dcd92ee
Esteban V.M. <estebanvm90@outlook.com>
2025-10-01 22:36:09 -0300

    update tabs layout
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/channel/[channelId]/@tabs/layout.tsx
+++ src/app/channel/[channelId]/@tabs/layout.tsx
@@ -1,10 +1,8 @@
 import { Tabs } from 'rsc-daisyui'
 
-export type TabsLayoutProps = LayoutProps<'/channel/[channelId]'>
+export type TabsLayoutProps = LayoutProps<'/'>
 
-export default async function TabsLayout({ children, params }: TabsLayoutProps) {
-  const { channelId: _ } = await params
-
+export default function TabsLayout({ children }: TabsLayoutProps) {
   return (
     <>
       <Tabs className='w-full p-0.5 text-center md:p-1.5' decorate='border'>
@@ -36,6 +34,7 @@ export default async function TabsLayout({ children, params }: TabsLayoutProps) 
         <Tabs.Tab aria-label='Community' as='input' className='w-[calc(1/7*100%)]' name='my_tabs_2' type='radio' />
         <Tabs.Content className='border-base-300 bg-base-100 p-10'>Tab content 7</Tabs.Content>
       </Tabs>
+
       {children}
     </>
   )
