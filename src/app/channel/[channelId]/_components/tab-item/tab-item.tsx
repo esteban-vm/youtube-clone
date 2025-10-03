b52d8903148ad86ac18690807cc9871791610d97
Esteban V.M. <estebanvm90@outlook.com>
2025-10-03 09:08:33 -0300

    update tab item
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/channel/[channelId]/_components/tab-item/tab-item.tsx
+++ src/app/channel/[channelId]/_components/tab-item/tab-item.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { Tabs } from 'rsc-daisyui'
 import tw from 'tailwind-styled-components'
 
 export function TabItem({ label, link, children }: Props.TabItem) {
+  const router = useRouter()
   const pathname = usePathname()
+
+  const handleClick = () => router.replace(link)
 
   return (
     <>
-      <Tabs.Tab active={pathname === link} as='span' className='w-[calc(1/7*100%)]'>
-        <Link href={link}>{label}</Link>
+      <Tabs.Tab active={pathname === link} as='button' className='w-[calc(1/7*100%)]' onClick={handleClick}>
+        {label}
       </Tabs.Tab>
       <Content>{children}</Content>
     </>
