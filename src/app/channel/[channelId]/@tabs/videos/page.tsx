de476dcc003f6cab2418f052186f78d7828071ba
Esteban V.M. <estebanvm90@outlook.com>
2025-10-03 15:19:08 -0300

    working on channel videos page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/channel/[channelId]/@tabs/videos/page.tsx
+++ src/app/channel/[channelId]/@tabs/videos/page.tsx
@@ -1,12 +1,44 @@
+'use client'
+
+import { use } from 'react'
+import tw from 'tailwind-styled-components'
+import { VideoGrid } from '@/channel/components'
+import { useFetchChannels, useFetchPlayLists } from '@/hooks'
+
 export type VideosPageProps = PageProps<'/channel/[channelId]/videos'>
 
-export default async function VideosPage({ params }: VideosPageProps) {
-  const { channelId } = await params
+export default function VideosPage({ params }: VideosPageProps) {
+  const { channelId } = use(params)
+
+  const { data: channels, isSuccess: isSuccessChannels } = useFetchChannels({
+    params: { id: channelId },
+    queryKey: [VideosPage.name, `CHANNEL ID: ${channelId}`],
+  })
+
+  const playlistId = channels?.items[0].contentDetails.relatedPlaylists.uploads ?? ''
+
+  const { data: playlists, isSuccess: isSuccessPlaylists } = useFetchPlayLists({
+    params: { playlistId, maxResults: '20' },
+    queryKey: [VideosPage.name, `UPLOAD PLAYLIST ID: ${playlistId}`],
+  })
+
+  if (!isSuccessChannels || !isSuccessPlaylists) return null
+
+  const playlistItems = playlists.items
 
   return (
-    <>
-      <p>Página de vídeos</p>
-      <p>{channelId}</p>
-    </>
+    <div>
+      <Title>Último vídeo</Title>
+      {playlistItems.length ? <VideoGrid playlistItems={playlistItems.slice(0, 1)} /> : <span>Sin vídeos</span>}
+      <Title>Vídeos del canal</Title>
+      {playlistItems.length ? <VideoGrid playlistItems={playlistItems} /> : <span>Sin vídeos</span>}
+    </div>
   )
 }
+
+const Title = tw.h2`
+  mb-3.5
+  text-left
+  text-lg
+  font-bold
+`
