0928fc385b45c17dd6ef009b00e031c5f358b35d
Esteban V.M. <estebanvm90@outlook.com>
2025-10-03 09:08:21 -0300

    add new tab items
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/constants/tab-items.ts
+++ src/constants/tab-items.ts
@@ -4,11 +4,11 @@ export const getTabItems = (channelId: string) => {
   const tabItems: Props.TabItem[] = [
     { id: crypto.randomUUID(), label: 'Inicio', link: helpers.typedRoute(`/channel/${channelId}`) },
     { id: crypto.randomUUID(), label: 'Vídeos', link: helpers.typedRoute(`/channel/${channelId}/videos`) },
-    // { id: crypto.randomUUID(), label: 'Cortos', getLink: helpers.typedRoute(`/channel/${channelId}`) },
-    // { id: crypto.randomUUID(), label: 'En vivo', getLink: helpers.typedRoute(`/channel/${channelId}`) },
-    // { id: crypto.randomUUID(), label: 'Podcasts', getLink: helpers.typedRoute(`/channel/${channelId}`) },
-    // { id: crypto.randomUUID(), label: 'Listas', getLink: helpers.typedRoute(`/channel/${channelId}`) },
-    // { id: crypto.randomUUID(), label: 'Comunidad', getLink: helpers.typedRoute(`/channel/${channelId}`) },
+    { id: crypto.randomUUID(), label: 'Cortos', link: helpers.typedRoute(`/channel/${channelId}/shorts`) },
+    { id: crypto.randomUUID(), label: 'En vivo', link: helpers.typedRoute(`/channel/${channelId}/lives`) },
+    { id: crypto.randomUUID(), label: 'Podcasts', link: helpers.typedRoute(`/channel/${channelId}/podcasts`) },
+    { id: crypto.randomUUID(), label: 'Listas', link: helpers.typedRoute(`/channel/${channelId}/playlists`) },
+    { id: crypto.randomUUID(), label: 'Comunidad', link: helpers.typedRoute(`/channel/${channelId}/community`) },
   ]
 
   return tabItems
